71ddc4101d21a29066fadc3710ebea5177e5981a
Mohammad-Abdullah-Al-Noman <abdullah.noman083@gmail.com>
2020-08-07 01:35:34 +0600

    array-spilt-solve

---
+++ array-spilt.js
@@ -0,0 +1,9 @@
+const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const part = nums.slice(2, 5);
+const removed = nums.splice(2, 2, 22, 33);
+console.log(part);
+console.log(removed);
+console.log(nums);
+
+const together = nums.join(" ");
+console.log(together);
--- scope.js
+++ scope.js
@@ -2,7 +2,7 @@ let bonus = 21;
 
 function sum(a, b) {
     let result = a + b + bonus;
-    // console.log('scope:', result)
+    console.log('scope:', result)
     if (result > 4) {
         const mood = 'Happy';
         console.log(mood);
@@ -11,5 +11,5 @@ function sum(a, b) {
 }
 
 const total = sum(2, 3);
-// console.log(bonus);
-// console.log(total);
+console.log(bonus);
+console.log(total);
