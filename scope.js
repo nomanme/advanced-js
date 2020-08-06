93e3c144b4a7c7a288d4d149e92db1fb58985b7f
Mohammad-Abdullah-Al-Noman <abdullah.noman083@gmail.com>
2020-08-07 01:07:47 +0600

    scope-updated

---
+++ scope.js
@@ -0,0 +1,15 @@
+let bonus = 21;
+
+function sum(a, b) {
+    let result = a + b + bonus;
+    // console.log('scope:', result)
+    if (result > 4) {
+        const mood = 'Happy';
+        console.log(mood);
+    }
+    return result;
+}
+
+const total = sum(2, 3);
+// console.log(bonus);
+// console.log(total);
