30e53afbb8b5552c68d6126353abe6592697b267
Mohammad-Abdullah-Al-Noman <abdullah.noman083@gmail.com>
2020-08-06 23:22:52 +0600

    true-false

---
+++ true-false.js
@@ -0,0 +1,9 @@
+const age = 0;
+// const name = 'Solimullah'
+let name = 0;
+// console.log(name);
+if (name || name == 0) {
+    console.log('condition is true');
+} else {
+    console.log('condition is false');
+}
