b3a284a7501a321173eff12ddcfcffb84a82ded6
Mohammad-Abdullah-Al-Noman <abdullah.noman083@gmail.com>
2020-08-06 23:39:49 +0600

    null-added

---
+++ null-undefined.js
@@ -0,0 +1,19 @@
+let pakhi;
+
+function add(num1, num2) {
+    console.log(num1 + num2);
+    return;
+}
+
+function add(num1, num2) {
+    console.log(num1, num2);
+}
+
+const premika = { name: 'smart baby', phone: 2323435 };
+console.log(premika.gf);
+
+let fun = undefined;
+console.log(fun);
+
+let ages = [2, 3, 4];
+console.log(ages[3]);
